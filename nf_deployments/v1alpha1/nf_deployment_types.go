d42078aea83e769d581f17c8477266d8e1ba41b2
Wim Henderickx <wim.henderickx@gmail.com>
2023-04-13 20:48:17 +0200

    aligned yaml/json tags

--- nf_deployments/v1alpha1/nf_deployment_types.go
+++ nf_deployments/v1alpha1/nf_deployment_types.go
@@ -152,24 +152,24 @@ type BGPNeighbor struct {
 // NFDeploymentStatus defines the observed state of nf deployment
 type NFDeploymentStatus struct {
 	// The generation observed by the deployment controller.
-	ObservedGeneration int32 `json:"observedGeneration"`
+	ObservedGeneration int32 `json:"observedGeneration" yaml:"observedGeneration"`
 	// Current service state of the NF.
-	Conditions []Condition `json:"conditions,omitempty"`
+	Conditions []Condition `json:"conditions,omitempty" yaml:"conditions,omitempty"`
 }
 
 type Condition struct {
 	// Type defines the type of the nf deployment condition.
-	Type NFDeploymentConditionType `json:"type"`
+	Type NFDeploymentConditionType `json:"type" yaml:"type"`
 	// Status defines the status of the condition, one of True, False, Unknown.
-	Status corev1.ConditionStatus `json:"status"`
+	Status corev1.ConditionStatus `json:"status" yaml:"status"`
 	// LastProbeTime defines the last time this condition was probed for.
-	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty" yaml:"lastProbeTime,omitempty"`
 	// LastTransitionTime defines the last time the condition transitioned from one status to another.
-	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty" yaml:"lastTransitionTime,omitempty"`
 	// Reason defines the reason for the condition's last transition.
-	Reason string `json:"reason,omitempty"`
+	Reason string `json:"reason,omitempty" yaml:"reason,omitempty"`
 	// Message defines a human readable message indicating details about the transition.
-	Message string `json:"message,omitempty"`
+	Message string `json:"message,omitempty" yaml:"message,omitempty"`
 }
 
 type NFDeploymentConditionType string
