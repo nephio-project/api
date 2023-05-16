688a329993930d503c5a507bd0590c9201cb9126
Wim Henderickx <wim.henderickx@gmail.com>
2023-05-16 15:11:12 +0200

    added token for repository

---
+++ infra/v1alpha1/token_interfaces.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2023 The Nephio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+// GetCondition returns the condition based on the condition kind
+func (r *Token) GetCondition(t ConditionType) Condition {
+	return r.Status.GetCondition(t)
+}
+
+// SetConditions sets the conditions on the resource. it allows for 0, 1 or more conditions
+// to be set at once
+func (r *Token) SetConditions(c ...Condition) {
+	r.Status.SetConditions(c...)
+}
+
+func (r *Token) GetTokenName() string {
+	if r.Namespace == "" || r.Namespace == "default" {
+		return r.Name
+	}
+	return r.Name + "-" + r.Namespace
+}
