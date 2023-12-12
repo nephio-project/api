886e456bc8fb5d7971b78ebf35beb7d4b20fb114
Stephen Wong <stephen.kf.wong@gmail.com>
2023-12-12 23:24:29 +0000

    address review comments

--- nf_topology/v1alpha1/nf_deployed.go
+++ nf_topology/v1alpha1/nf_deployed.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,26 +16,22 @@ limitations under the License.
 package v1alpha1
 
 type NFConnectivity struct {
-	// peer NF's Id (see NFInstance struct below)
-	NeighborName string `json:"neighborName,omitempty" yaml:"neighborName,omitempty"`
+    // peer NF's Id (see NFInstance struct below)
+    NeighborName string `json:"neighborName,omitempty" yaml:"neighborName,omitempty"`
 }
 
 // NFDeployedInstance defines an NF instance that is deployed
 type NFDeployedInstance struct {
-	// unique ID for this NF instance
-	ID string `json:"id,omitempty" yaml:"id,omitempty"`
-	// name of workload cluster where the NF instance is to be deployed
-	ClusterName string `json:"clusterName,omitempty" yaml:"clusterName,omitempty"`
-	// type of NF, example: amf, smf, upf
-	NFType string `json:"nfType,omitempty" yaml:"nfType,omitempty"`
-	/*
-		    TODO(s3wong): the following parameters should eventually be metadata embedded
-		    in a NF package (NFType should be as well)
-			// NF vendor name
-			NFVendor string `json:"nfVendor,omitempty" yaml:"nfVendor,omitempty"`
-			// the software version of this NF vendor's NFType
-			NFVersion string `json:"nfVersion,omitempty" yaml:"nfVersion,omitempty"`
-	*/
-	// list of connected NF instances to this NF instance
-	Connectivities []NFConnectivity `json:"connectivities,omitempty" yaml:"connectivities,omitempty"`
+    // unique ID for this NF instance
+    ID string `json:"id,omitempty" yaml:"id,omitempty"`
+    // name of workload cluster where the NF instance is to be deployed
+    ClusterName string `json:"clusterName,omitempty" yaml:"clusterName,omitempty"`
+    // type of NF, example: amf, smf, upf
+    NFType string `json:"nfType,omitempty" yaml:"nfType,omitempty"`
+    // NF vendor name
+    NFVendor string `json:"nfVendor,omitempty" yaml:"nfVendor,omitempty"`
+    // the software version of this NF vendor's NFType
+    NFVersion string `json:"nfVersion,omitempty" yaml:"nfVersion,omitempty"`
+    // list of connected NF instances to this NF instance
+    Connectivities []NFConnectivity `json:"connectivities,omitempty" yaml:"connectivities,omitempty"`
 }
--- nf_topology/v1alpha1/nf_topology_types.go
+++ nf_topology/v1alpha1/nf_topology_types.go
@@ -17,80 +17,80 @@ limitations under the License.
 package v1alpha1
 
 import (
-	nephiodeployv1alpha1 "github.com/nephio-project/api/nf_deployments/v1alpha1"
-	nephioreqv1alpha1 "github.com/nephio-project/api/nf_requirements/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    nephiodeployv1alpha1 "github.com/nephio-project/api/nf_deployments/v1alpha1"
+    nephioreqv1alpha1 "github.com/nephio-project/api/nf_requirements/v1alpha1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NFInterface defines the specification of network attachment points of a NF
 type NFInterface struct {
-	// Name of the network attachment point
-	Name string `json:"name" yaml:"name"`
+    // Name of the network attachment point
+    Name string `json:"name" yaml:"name"`
 
-	// NetworkInstanceRef is a reference to NetworkInstance. Two NF with attachment to
-	// the same NetworkInstance is considered connected neighbors
-	NetworkInstanceName string `json:"networkInstanceName" yaml:"networkInstanceName"`
+    // NetworkInstanceRef is a reference to NetworkInstance. Two NF with attachment to
+    // the same NetworkInstance is considered connected neighbors
+    NetworkInstanceName string `json:"networkInstanceName" yaml:"networkInstanceName"`
 }
 
 // PackageRevisionReference is a temporary replica of PackageRevisionReference used for the
 // ONE Summit
 type PackageRevisionReference struct {
-	// Namespace is the namespace for both the repository and package revision
-	// +optional
-	Namespace string `json:"namespace,omitempty"`
+    // Namespace is the namespace for both the repository and package revision
+    // +optional
+    Namespace string `json:"namespace,omitempty"`
 
-	// Repository is the name of the repository containing the package
-	RepositoryName string `json:"repository"`
+    // Repository is the name of the repository containing the package
+    RepositoryName string `json:"repository"`
 
-	// PackageName is the name of the package for the revision
-	PackageName string `json:"packageName"`
+    // PackageName is the name of the package for the revision
+    PackageName string `json:"packageName"`
 
-	// Revision is the specific version number of the revision of the package
-	Revision string `json:"revision"`
+    // Revision is the specific version number of the revision of the package
+    Revision string `json:"revision"`
 }
 
 // NFTemplate defines the template for deployment of an instance of a NF
 type NFTemplate struct {
-	// NFType specifies the type of NF this template is specifying
-	NFType string `json:"nfType" yaml:"nfType"`
+    // NFType specifies the type of NF this template is specifying
+    NFType string `json:"nfType" yaml:"nfType"`
 
-	// NFPackageRef specifies the upstream package reference for this NFTemplate
-	NFPackageRef PackageRevisionReference `json:"nfPackageRef" yaml:"nfPackageRef"`
+    // NFPackageRef specifies the upstream package reference for this NFTemplate
+    NFPackageRef PackageRevisionReference `json:"nfPackageRef" yaml:"nfPackageRef"`
 
-	// Capacity specifies the NF capacity profile for this NF instance
-	Capacity nephioreqv1alpha1.CapacitySpec `json:"capacity,omitempty" yaml:"capacity,omitempty"`
+    // Capacity specifies the NF capacity profile for this NF instance
+    Capacity nephioreqv1alpha1.CapacitySpec `json:"capacity,omitempty" yaml:"capacity,omitempty"`
 
-	// NFInterfaces
-	NFInterfaces []NFInterface `json:"nfInterfaces,omitempty" yaml:"nfInterfaces,omitempty"`
+    // NFInterfaces
+    NFInterfaces []NFInterface `json:"nfInterfaces,omitempty" yaml:"nfInterfaces,omitempty"`
 }
 
 type NFInstance struct {
-	// Name specifies the name of this NFInstance
-	Name string `json:"name" yaml:"name"`
+    // Name specifies the name of this NFInstance
+    Name string `json:"name" yaml:"name"`
 
-	// ClusterSelector specifies the matching labels for the NF instance to be instantiated
-	ClusterSelector metav1.LabelSelector `json:"clusterSelector" yaml:"clusterSelector"`
+    // ClusterSelector specifies the matching labels for the NF instance to be instantiated
+    ClusterSelector metav1.LabelSelector `json:"clusterSelector" yaml:"clusterSelector"`
 
-	// NFTemplate specifies the template of the NF to be deployed when a cluster matches
-	// the selector above
-	NFTemplate NFTemplate `json:"nfTemplate" yaml:"nfTemplate"`
+    // NFTemplate specifies the template of the NF to be deployed when a cluster matches
+    // the selector above
+    NFTemplate NFTemplate `json:"nfTemplate" yaml:"nfTemplate"`
 }
 
 // NFTopologySpec defines the specification of NFTopology
 type NFTopologySpec struct {
-	NFInstances []NFInstance `json:"nfInstances" yaml:"nfInstances"`
+    NFInstances []NFInstance `json:"nfInstances" yaml:"nfInstances"`
 }
 
 // NFTopologyStatus defines the observed state of NFTopology
 type NFTopologyStatus struct {
-	// Number of NFs deployed for this topology
-	NumNFDeployed int32 `json:"numNFDeployed,omitempty"`
+    // Number of NFs deployed for this topology
+    NumNFDeployed int32 `json:"numNFDeployed,omitempty"`
 
-	// Current service state of the NF.
-	Conditions []nephiodeployv1alpha1.NFDeploymentConditionType `json:"conditions,omitempty"`
+    // Current service state of the NF.
+    Conditions []nephiodeployv1alpha1.NFDeploymentConditionType `json:"conditions,omitempty"`
 
-	// Detail on the deployed instances.
-	NFInstances []NFDeployedInstance `json:"nfInstances,omitempty" yaml:"nfInstances,omitempty"`
+    // Detail on the deployed instances.
+    NFInstances []NFDeployedInstance `json:"nfInstances,omitempty" yaml:"nfInstances,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -98,18 +98,18 @@ type NFTopologyStatus struct {
 
 // NFTopology is the Schema for the nfTopology API
 type NFTopology struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   NFTopologySpec   `json:"spec,omitempty"`
-	Status NFTopologyStatus `json:"status,omitempty"`
+    Spec   NFTopologySpec   `json:"spec,omitempty"`
+    Status NFTopologyStatus `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 
 // NFTopologyList contains a list of NFTopology
 type NFTopologyList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []NFTopology `json:"items"`
+    metav1.TypeMeta `json:",inline"`
+    metav1.ListMeta `json:"metadata,omitempty"`
+    Items           []NFTopology `json:"items"`
 }
